2801f7fd73b83e6390362a90fde667654cac6058
greaterthanprougraff <patrick.rougraff@accenture.com>
2025-05-22 01:42:19 -0400

    Day 2

--- week-01-introduction-to-coding/src/main/java/org/accenture/day02/lessons/Lesson01Operators.java
+++ week-01-introduction-to-coding/src/main/java/org/accenture/day02/lessons/Lesson01Operators.java
@@ -8,10 +8,19 @@ public class Lesson01Operators {
     public static void main(String[] args) {
         //Reviewing operators from Day01
         //TODO Sum of two variables
+        int b = 1;
+        int c = 2;
+        int sum = b + c;
+        System.out.println(sum);
 
         //TODO Get the remainder when you divide 12 by 10 and store to variable named 'remainder'
-
+        int remainder = 12 % 10;
+        System.out.println(remainder);
         //TODO Increase the value of a using '++' until it equals 5
         int a = 1;
+        while (a != 5) {
+            a++;
+        }
+        System.out.println(a);
     }
 }
--- week-01-introduction-to-coding/src/main/java/org/accenture/day02/worksheet/Day02Worksheet.java
+++ week-01-introduction-to-coding/src/main/java/org/accenture/day02/worksheet/Day02Worksheet.java
@@ -4,61 +4,80 @@ public class Day02Worksheet {
 
     // Return true if both numbers are positive
     public static boolean bothPositive(int x, int y) {
-        return false;
+        return x > 0 && y > 0;
     }
 
     // Return true if exactly one number is positive
     public static boolean onlyOnePositive(int x, int y) {
-        return false;
+        return (x > 0 && y <= 0) || (x <= 0 && y > 0);
     }
 
     // Return true if person is eligible to vote
     public static boolean canVote(boolean isCitizen, int age) {
-        return false;
+        return isCitizen && age >= 18;
     }
 
     // Return true if year is a leap year (divisible by 4, not 100 unless also 400)
     public static boolean isLeapYear(int year) {
-        return false;
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
 
     // Return grade letter based on score
     public static String gradeLetter(int score) {
-        return "";
+        if (score >= 90) return "A";
+        else if (score >= 80) return "B";
+        else if (score >= 70) return "C";
+        else if (score >= 60) return "D";
+        else return "F";
     }
 
     // Return "positive", "negative", or "zero"
     public static String numberSign(int x) {
-        return "";
+        if (x > 0) return "positive";
+        else if (x < 0) return "negative";
+        else return "zero";
     }
 
     // Return true if number is between 10 and 20 (inclusive)
     public static boolean isBetween10And20(int x) {
-        return false;
+        return x >= 10 && x <= 20;
     }
 
     // Return true if number is divisible by 3 or 5
     public static boolean isMultipleOfThreeOrFive(int x) {
-        return false;
+        return x % 3 == 0 || x % 5 == 0;
     }
 
     // Return "Fizz" if divisible by 3, "Buzz" if divisible by 5, "FizzBuzz" if both, else number as string
     public static String fizzBuzz(int x) {
-        return "";
+        if (x % 3 == 0 && x % 5 == 0) return "FizzBuzz";
+        else if (x % 3 == 0) return "Fizz";
+        else if (x % 5 == 0) return "Buzz";
+        else return Integer.toString(x);
     }
 
     // Return true if sum of x and y is even
     public static boolean isSumEven(int x, int y) {
-        return false;
+        return (x + y) % 2 == 0;
     }
 
     // Return the larger of two numbers
     public static int max(int x, int y) {
-        return 0;
+        if (x > y) {
+            return x;
+        } else {
+            return y;
+        }
+        
     }
 
     // Return the smaller of two numbers
     public static int min(int x, int y) {
-        return 0;
+        if (x < y) {
+            return x;
+        } else {
+            return y;
+        }
+        
     }
 }
